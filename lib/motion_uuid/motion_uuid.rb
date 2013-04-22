f293eca5f5ea09b417240615e786ae2957727b35
Tres Trantham <tres@trestrantham.com>
2013-04-21 20:42:40 -0400

    Rubymotion modifications

--- lib/motion_uuid/motion_uuid.rb
+++ lib/motion_uuid/motion_uuid.rb
@@ -1,37 +1,5 @@
-# monkey-patch Friendly::UUID to serialize UUIDs
 module UUIDTools
-  module UUID
-    alias_method :id, :raw
-
-    # duck typing activerecord 3.1 dirty hack )
-    def gsub *; self; end
-
-    def quoted_id
-      s = raw.unpack("H*")[0]
-      "x'#{s}'"
-    end
-
-    def as_json(options = nil)
-      hexdigest.upcase
-    end
-
-    def to_param
-      hexdigest.upcase
-    end
-
-    def self.serialize(value)
-      case value
-      when self
-        value
-      when String
-        parse_string value
-      else
-        nil
-      end
-    end
-
-  private
-
+  class UUID
     def self.parse_string(str)
       return nil if str.length == 0
       if str.length == 36
@@ -46,52 +14,19 @@ module UUIDTools
 end
 
 module MotionUUID
-  module UUID
+  class UUID
+    @@uuid_namespace = nil
 
-    included do
-      class_attribute :_uuid_namespace, instance_writer: false
-      class_attribute :_uuid_generator, instance_writer: false
-      self._uuid_generator = :random
-
-      singleton_class.alias_method_chain :instantiate, :uuid
+    def self.uuid_namespace(namespace)
+      namespace = UUIDTools::UUID.parse_string(namespace) unless namespace.is_a? UUID
+      @@uuid_namespace = namespace
     end
 
-    module ClassMethods
-      def natural_key(*attributes)
-        self._natural_key = attributes
-      end
-
-      def uuid_namespace(namespace)
-        namespace = UUIDTools::UUID.parse_string(namespace) unless namespace.is_a? UUIDTools::UUID
-        self._uuid_namespace = namespace
-      end
-
-      def uuid_generator(generator_name)
-        self._uuid_generator = generator_name
-      end
-
-      def instantiate_with_uuid(record)
-        uuid_columns.each do |uuid_column|
-          record[uuid_column] = UUIDTools::UUID.serialize(record[uuid_column]).to_s if record[uuid_column]
-        end
-        instantiate_without_uuid(record)
-      end
-
-      def uuid_columns
-        @uuid_columns ||= columns.select { |c| c.type == :uuid }.map(&:name)
-      end
-    end
-
-    def create_uuid(salt = nil)
-      if salt
-        UUIDTools::UUID.sha1_create(_uuid_namespace || UUIDTools::UUID_OID_NAMESPACE, salt)
+    def self.create_uuid(text = nil)
+      if text
+        UUIDTools::UUID.sha1_create(@@uuid_namespace || UUIDTools::NameSpace_OID, text)
       else
-        case _uuid_generator
-        when :random
-          UUIDTools::UUID.random_create
-        when :time
-          UUIDTools::UUID.timestamp_create
-        end
+        UUIDTools::UUID.random_create
       end
     end
   end
